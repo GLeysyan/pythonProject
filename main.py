0e1184074faef10d226bf2f93e0e8a85057278aa
Leysyan Gubaydullina <leysyan.gubaydullina@gmail.com>
2021-12-25 14:58:48 +0100

    Second commit with 1 file

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 # Adding another line here
+# Adding another line
 if __name__ == '__main__':
     print_hi('PyCharm')
 
